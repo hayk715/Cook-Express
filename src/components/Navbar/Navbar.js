af5282731ed810caf343b3df5f570c610ca941a2
hayk715 <hayk715@gmail.com>
2018-10-20 13:46:20 -0700

    home page markup

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-const Navbar = props => (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Cook Express
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/home"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+const SimpleNavbar = props => (
+<Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#brand">Cook Express</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        Log In
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Sign Up
+      </NavItem>
+      <NavItem eventKey={3} href="#">
+        About
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+
   );
   
-  export default Navbar;
+  export default SimpleNavbar;
