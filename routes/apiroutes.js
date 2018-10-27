61964a5f24bad9b6203dfdcd1861d2beed97dde5
hayk715 <hayk715@gmail.com>
2018-10-27 10:39:26 -0700

    food api hidden

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,26 +1,18 @@
 const unirest = require("unirest");
 const router = require("express").Router();
-
+const keys = require("../config/keys/keys");
 
 router.get("/recipes", (req, res) => {
   console.log(req.query)
   unirest
     .get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?query=${req.query.q}&limitLicense=false&ranking=2&offset=3&number=10&instructionsRequired=true`)
-    .header("X-Mashape-Key", "PmLhGIV6EbmshUTASHgOvs7KY0Jbp1LtMz0jsnYKJHJACqylp0")
+    .header("X-Mashape-Key", keys.food.foodkey)
     .header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
     .end(function (result) {
   console.log(result.body);
       res.send(result.body);
 });
-
-
-unirest.get(`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${req.query.id}/information`)
-.header("X-Mashape-Key", "PmLhGIV6EbmshUTASHgOvs7KY0Jbp1LtMz0jsnYKJHJACqylp0")
-.header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
-.end(function (result) {
-  console.log(result.body);
-  res.send(result.body);
-});
 })
+
 
 module.exports = router;
