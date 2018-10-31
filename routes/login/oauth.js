2014cc9b940261564fa1084b40a9ea834835503a
Trav <trscottjr@gmail.com>
2018-10-30 19:53:00 -0700

    login and profile

--- routes/login/oauth.js
+++ routes/login/oauth.js
@@ -1,46 +1,38 @@
 const router=require('express').Router();
 const passport=require("passport");
-
-
-
 // Routes for Google OAuth 
 router.get('/login/google',function(req, res, next) { console.log('hi'); next(); },
   passport.authenticate('google', { scope: ['profile'] }));
-
 // Callback route for Google after Authentication 
 router.get('/login/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
+      // res.send(req.user.id)
       if (process.env.NODE_ENV === 'production') {
-        res.redirect('/profile/logged');
+        res.redirect('/profile');
       } else {
-          res.redirect('http://localhost:3000/profile/logged');
+          res.redirect('http://localhost:3000/profile');  
       }
   });
-
   // Route for Twitter OAuth
   router.get("/login/twitter", passport.authenticate("twitter"));
-
   // Callback Route for Twitter after Authentication
   router.get('/login/twitter/callback', 
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
     console.log('hello');
-
     res.redirect("/profile");
   });
-
-
-  router.get('/charliesroute', function(req, res) {
-    debugger;
+  router.get('/loggedin', function(req, res) {
+    // debugger;
     if (req.isAuthenticated()) {
-      return res.json({
-        username: 'hi'
-      })
+      var userData={
+        name:req.user.username,
+        id:req.user.id
+      }
+      return res.json(userData);
     }
   })
-
  
-
 module.exports=router;
